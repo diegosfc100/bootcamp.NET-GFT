5fe377882f592fac1fa0842564f73b0d28424dec
Diego Rodrigues <diego.sfc100@hotmail.com>
2022-01-21 11:10:58 -0300

    Add new Hero

--- Abstraindo-JogoRPG-OOP-Csharp/Program.cs
+++ Abstraindo-JogoRPG-OOP-Csharp/Program.cs
@@ -1,14 +1,17 @@
-﻿using Abstraingo_JogoRPG_OOP_Csharp.src.Entities;
+﻿using Abstraindo_JogoRPG_OOP_Csharp.src.Entities;
 
     class Program
     {
         static void Main(string[] args)
         {
-            Hero hero = new Hero("Arus", 23, "Knight");
-            Wizard wizard = new Wizard("Thanos", 99, "Devil");
+            Knight arus = new Knight("Arus", 23, "Knight", 444, 486, 768, 778);
+            Wizard wizard = new Wizard("Thanos", 99, "Devil", 369, 410, 845, 976);
+            Ninja scarlet = new Ninja("Scarlet", 80, "Ninja", 330, 350, 715, 799);
 
-            Console.WriteLine(hero.Attack());
-            Console.WriteLine(wizard.Attack());
+            Console.WriteLine(arus.Attack());
+            Console.WriteLine(wizard.Attack(7));
+            Console.WriteLine(scarlet);
+
         }
     }
 
