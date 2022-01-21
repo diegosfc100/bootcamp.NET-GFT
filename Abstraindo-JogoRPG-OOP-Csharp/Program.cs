5247888086198c954d8b1de44794c9aa58a654c0
Diego Rodrigues <diego.sfc100@hotmail.com>
2022-01-20 23:58:31 -0300

    Initial Commit

---
+++ Abstraindo-JogoRPG-OOP-Csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Abstraingo_JogoRPG_OOP_Csharp.src.Entities;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero hero = new Hero("Arus", 23, "Knight");
+            Wizard wizard = new Wizard("Thanos", 99, "Devil");
+
+            Console.WriteLine(hero.Attack());
+            Console.WriteLine(wizard.Attack());
+        }
+    }
+
---
+++ Abstraindo-JogoRPG-OOP-Csharp/src/Entities/Wizard.cs
@@ -0,0 +1,16 @@
+namespace Abstraingo_JogoRPG_OOP_Csharp.src.Entities
+{
+    public class Wizard : Hero
+    {
+        public Wizard(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public override string Attack(){
+            return this.Name + " lançou magia";
+        }        
+    }
+}
