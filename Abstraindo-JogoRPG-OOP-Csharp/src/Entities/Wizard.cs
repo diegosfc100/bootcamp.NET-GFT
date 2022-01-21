39e5d941396c119773a8e215c4912b99e896f027
Diego Rodrigues <diego.sfc100@hotmail.com>
2022-01-21 11:13:08 -0300

    Update new attack

--- Abstraindo-JogoRPG-OOP-Csharp/src/Entities/Wizard.cs
+++ Abstraindo-JogoRPG-OOP-Csharp/src/Entities/Wizard.cs
@@ -1,16 +1,30 @@
-namespace Abstraingo_JogoRPG_OOP_Csharp.src.Entities
+namespace Abstraindo_JogoRPG_OOP_Csharp.src.Entities
 {
     public class Wizard : Hero
     {
-        public Wizard(string Name, int Level, string HeroType)
+        public Wizard(string Name, int Level, string HeroType, int HPmin, int HPmax, int MPmin, int MPmax) : base(Name, Level, HeroType, HPmin, HPmax, MPmin, MPmax)
         {
             this.Name = Name;
             this.Level = Level;
             this.HeroType = HeroType;
+            this.HPmin = HPmin;
+            this.Name = Name;
+            this.HPmax = HPmax;
+            this.MPmin = MPmin;
+            this.MPmax = MPmax;  
         }
 
         public override string Attack(){
             return this.Name + " lançou magia";
-        }        
+        }  
+
+        public string Attack(int Bonus){
+            
+            if(Bonus > 6){
+                return this.Name + " lançou magia super afetiva com bonus de " + Bonus;
+            }else{
+                return this.Name + " lançou magia com bonus de ataque de " + Bonus;
+            }            
+        }      
     }
 }
