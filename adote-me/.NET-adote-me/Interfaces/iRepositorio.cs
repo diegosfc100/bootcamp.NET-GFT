dcf52b91e35197b66c66bc4efebcfeaf12181d39
Diego Rodrigues <diego.sfc100@hotmail.com>
2022-01-24 20:29:27 -0300

    Adiciona Interface padrão

---
+++ adote-me/.NET-adote-me/Interfaces/iRepositorio.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+
+public interface iRepositorio<T>
+{
+    List<T> Lista();
+    T RetornaPorId( int id);
+    void Insere(T entidade);
+    void Exclui(int id);
+    void Atualiza(int id, T entidade);
+    int ProximoId();
+}
