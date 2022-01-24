fe8e94d5f7842008a273c400cb4410fcc6ae5e93
Diego Rodrigues <diego.sfc100@hotmail.com>
2022-01-24 12:19:04 -0300

    Configura método de opções

--- adote-me/.NET-adote-me/Program.cs
+++ adote-me/.NET-adote-me/Program.cs
@@ -4,4 +4,26 @@
     {
         
     }
+
+    private static string ObterOpcaoUsuario()
+    {
+
+    Console.WriteLine();
+    Console.WriteLine("dotNET Adoção a seu dispor =) ");
+    Console.WriteLine("Informe a opção desejda: ");
+
+    Console.WriteLine("1- Listar Pets");
+    Console.WriteLine("2- Inserir Pet para doação");
+    Console.WriteLine("3- Atualizar lista de Pets");
+    Console.WriteLine("4- Excluir Pet adotado");
+    Console.WriteLine("5- Visualizar Pet");
+    Console.WriteLine("C- Limpar tela");
+    Console.WriteLine("X- Sair");
+    Console.WriteLine();
+   
+    string opcaoUsuario = Console.ReadLine().ToUpper();
+    Console.WriteLine();
+    return opcaoUsuario;
+    }
+    
 }
