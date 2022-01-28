e4e0d11c77c78683ced9f507c335ae8de465a137
Diego Rodrigues <diego.sfc100@hotmail.com>
2022-01-27 23:04:17 -0300

    Funções

--- Hello, World !/Program.cs
+++ Hello, World !/Program.cs
@@ -11,35 +11,68 @@ using System.Threading.Tasks;
 
 namespace HelloWorld 
 { 
-    // Início do programa.
+    
     class Program 
     {
         
         static void Main(string[] args) // Função principal - Qdo um usuário cria um .exe e executa ele, o primeiro cod q irá rodar é oq estiver dentro dessa função principal
         {
-            Console.Write("Hello World!"); // Comando para escrever
-            Console.WriteLine("Rumo à GFT"); // WriteLine faz o mesmo q o Write porém tem a quebra de linha no final do texto.
-                        
+                /* C O N S O L E */
+
+            // Console.Write(""); 
+            // Escreve o texto na mesma linha, por exemplo:
+            Console.Write("Olá ");
+            Console.Write("Mundo");
+            // Olá Mundo
+
+            // Console.WriteLine("");
+            // Escreve o texto e logo a seguir quebra para a linha seguinte, por exemplo:
+            Console.WriteLine();
+            Console.WriteLine("Olá ");
+            Console.WriteLine("Mundo");
+            // Olá 
+            // Mundo
+
+
+            // Capturando entrada do usuário com CONSOLE.READLINE();
+            string profissao;
+            Console.WriteLine("Escreva sua profissão: ");
+            profissao = Console.ReadLine();
+            Console.WriteLine($"Sua profissão é: {profissao}");
+
+
+            // No C# tudo que é digitado no console é tratado como texto
+            // Nesse caso usamos o int.Parse para converter a string em Inteiro
+            int primo;
+            Console.WriteLine("Digite um número primo: ");
+            primo = int.Parse(Console.ReadLine()); 
+            Console.WriteLine($"Você digitou o número: {primo}");
+
+           
+
+                /* V A R I A V E I S */
+
             // No C# as variáveis são declaradas com o tipo.
-            // Int (inteiro) ; Float (flutuante) ; Bool (true ou false) ; String ("Texto") ;  Char('a') caracter
-            // Regras de nomes de variáveis: tipo nome = valor;
-            /* A-Z ; não pode iniciar com número ; não pode conter caracter especias( @ $ .) ;  não pode conter palavras reservadas ex: class */
+            // int 
+            // float 
+            // bool 
+            // string 
+            // char('a') caracter
+
+            // tipo nome = valor;
+            // A-Z / Não pode iniciar com número / não pode conter caracter especias( @ $ .) 
 
             int nascimento = 1990;
             string sobreNome = "Rodrigues";
-            float peso = 79.500f; // variáveis do tipo float devem conter o 'f' no final do valor.
-            bool pesoPena = true;
+            float peso = 79.500f; // tipo float devem conter o 'f' no final do valor
+            bool magro = true;
 
-            Console.WriteLine("Meu peso atual é " + peso);
-
-            //atualizando um valor
-            peso = 80.00f;
-            Console.WriteLine("Meu peso atual é " + peso);
-
+            
             // Declarando variaveis omitindo seu tipo
             var nomeCompleto = "Diego Rodrigues";
             var idadeAdulto = 30;
             // uma vez declarada uma variavel com um certo tipo, ela permanece com o msm tipo pra sempre, não podendo ser alterado.
+
 
             //Forma mais avançada de declarar variáveis - Forma Dinamica
             dynamic corFavorita = "Azul";
@@ -48,92 +81,124 @@ namespace HelloWorld
             corFavorita = 2022; 
 
 
-            // Constantes -  parecidos com var porém uma vez declarada não possível alterar seu valor
-            const float PI = 3.14f;
+            // CONSTANTES -  parecidos com var porém uma vez declarada não possível alterar seu valor
+            const float PI = 3.14f; // Exemplo com valor de PI q não pode ser alterado.
 
  
-            /* Capturando entrada do usuário */
-            string profissao = "";
-            Console.WriteLine("Escreva sua profissão: ");
-            profissao = Console.ReadLine();
-            Console.WriteLine("Sua profissão é: " + profissao);
-
-            // No C# tudo que é digitado no console é tratado como texto.
-            int maiorIdade = int.Parse(Console.ReadLine()); // Nesse caso usamos o int.Parse para converter a string em Inteiro.
-
-
-            /* Operadores Aritiméticos */
-            int numeroQualquer = 20 + 20;
-            int numeroMult = 10 + 10 - 10 * 5; // multiplicação e divisão são feitos primeiro.
-            int numeroDiv = 5 / 2; // A divisão de números int, a variavel sempre vai armazenar o resultado int, no caso 2.
-            // para concertar isso, devemos definir pelo menos um dos valores como float 
-
-
-            /* Condicionais */
-            if(50 >= 15) // SE
-            {
-                Console.WriteLine("Sim");
-            }
-            else if ( 20 == 20) // SE NÂO SE
-            {
-                Console.WriteLine("É verdade");
-            }
-            else // SE NÃO
-            {
-                Console.WriteLine("Não");
-            }
-
-
-            /* Operadores Lógicos */
+                  
+                /* C O N D I C I O N A I S + OPERADORES LÓGICOS */
+                                         
             // && (E / AND) 
             // || (OU / OR)
 
 
-            int idade = int.Parse(Console.ReadLine());
-
+            int idade;
+            Console.WriteLine("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+            
             if (idade >= 0 && idade <= 11)
             {
-                Console.WriteLine("Vc é uma criança!");
+                Console.WriteLine("Vc é uma Criança!");
             }
             else if (idade >= 12 && idade <= 18)
             {
-                Console.WriteLine("Vc é um adolescente!");
+                Console.WriteLine("Vc é um Adolescente!");
             }
             else if (idade >= 10 && idade <= 60)
             {
-                Console.WriteLine("Vc é um adulto!");
+                Console.WriteLine("Vc é um Adulto!");
             }
             else
             {
-                Console.WriteLine("Vc é um idoso!");
+                Console.WriteLine("Vc é um Idoso!");
             }
             
-
-            /* Função */
-            FuncaoExibirMsg();            
             
+
+                /*   A R R A Y   */
+            
+            // string[] ~> array do tipo string
+            // int[]    ~> array do tipo inteiro
+            /* 2 formas de declarar um ARRAY: */
+            // int[] idades = new int[3] {20, 33, 51}; 
+            // int[] anos = {1990, 2002, 2010};
+            
+            string[] frutas = new string[5] {
+                "Laranja",
+                "Uva",
+                "Mamão",
+                "Melancia",
+                "Abacate"
+            };
+
+            Console.WriteLine(frutas[0]);
+            Console.WriteLine(frutas[4]);
+
+
+            //Sobrescrevendo valores
+            frutas[0] = "Lichia";
+            frutas[4] = "Banana";
+            Console.WriteLine(frutas[0]);
+            Console.WriteLine(frutas[4]);
+
+
+
+                /*   F U N Ç Õ E S   */
+
+            static void FuncaoExibirMsg(){ // void = vazia = não retorna nada.
+                Console.WriteLine("Exemplo Função part I");
+                Console.WriteLine("Exemplo Função part II");
+                Console.WriteLine("Exemplo Função part III");
+            }
+            // Chamando Função
+            FuncaoExibirMsg();
+
+
+                // Função COM PARAMETRO
+            // Parametros sao semelhantes a variaveis
+            // int = tipo / idade = nome do parametro
+            // string = tipo / nome = nome do parametro
+
+            static void ComParametro(string sobrenome,int idade ) 
+            {
+                Console.WriteLine(sobrenome + idade);
+            }
+
+            ComParametro("Rodrigues", 20 );
+
+         
+                // Função COM RETORNO   
+            // Console.ReadLine() é um exemplo de função com retorno.
+            // Qdo a função houver retorno, antes do nome dela deve vir o tipo de retorno, string/bool/int...
+
+            static int Somar(int a, int b, int c) 
+            {
+                int result = a + b + c;
+                return result;
+            }
+                          
+            Somar(10, 20, 30); // a função retornara o resultado mas não exibirá nada na tela.
+
+            // Uma forma de ver esse retorno é colocar a dunção no console.
+            int resultFuncao = Somar(10, 20, 70);
+            Console.WriteLine($"O resultado da função SOMAR é: {resultFuncao}"); 
+
+
+          
+            
+
         }
 
         
 
-         /* Funçoes */
-         /* exemplo 1 */        
-        static void FuncaoExibirMsg(){ // void = vazia = não retorna nada.
-            Console.WriteLine("Exemplo Função part I");
-            Console.WriteLine("Exemplo Função part II");
-            Console.WriteLine("Exemplo Função part III");
-        }  
-
-        /* exemplo 2 */
-        /* função com parametro / parametros sao semelhantes a variaveis */
-        static void GerarPreco(int idade, string nome) // int = tipo / idade = nome do parametro
-        {
-            Console.WriteLine(nome + idade);
-        }
+              
+        
 
         
-           
+
+
         
+
       }
     //Fim do programa.
     
